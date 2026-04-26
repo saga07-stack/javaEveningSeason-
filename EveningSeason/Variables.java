57941bf8ffc6a955d7c3615a732abbdac8e1427b
sagar poudel <poudelsagar@icloud.com>
2026-04-27 00:00:10 +0900

    lesson

---
+++ EveningSeason/Variables.java
@@ -0,0 +1,12 @@
+package EveningSeason;
+import java.util.*;
+public class Variables {
+    public static void main(String[] args){
+
+       
+
+
+        
+    }
+    
+}
---
+++ EveningSeason/Varibal.java
@@ -0,0 +1,13 @@
+package EveningSeason;
+
+public class Varibal {
+  int  age = 20;
+  double rate =1.1;
+   int minAge = 18;
+  char sex = 'f';
+  boolean isAdult = true ;
+  String name = "sagar";
+
+
+    
+}
