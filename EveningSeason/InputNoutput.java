1e1147b6c6eea6eb8c69dbbfc5afc070baaf9f8c
sagar poudel <poudelsagar@icloud.com>
2026-04-28 23:40:09 +0900

    practice

---
+++ EveningSeason/InputNoutput.java
@@ -0,0 +1,12 @@
+package EveningSeason;
+import java.util.*;
+//import java.util.Scanner;
+public class InputNoutput {
+    public static void main (String [] args){
+ Scanner src = new Scanner(System.in); //creating the object 
+ System.out.println("enter your age ");
+    int age = src.nextInt();
+    System.out.println(age+" is your age ");
+    }
+    
+}
---
+++ EveningSeason/printF.java
@@ -0,0 +1,11 @@
+package EveningSeason;
+
+public class printF {
+    public static void main(String[] args){
+    int age = 20;
+    double height = 20.2;
+    System.out.printf("age : %d",age);
+    System.out.printf("height : %.2f",height);
+    }
+    
+}
